26b7662119f95618a0e87217484becb8bdea42de
Yukishekinko <incocryptix@mail.ru>
2025-03-21 23:32:48 +0300

    add basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", sum(3,2));
+    println!("{}", division(6,2));
+}
+
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn division(a: i32, b: i32) -> i32 {
+    a / b
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{division, sum};
+
+    #[test]
+    fn sum_test() {
+        assert_eq!(sum(1, 2), 3);
+    }
+
+    #[test]
+    fn division_test() {
+        assert_eq!(division(6, 2), 3);
+    }
 }
